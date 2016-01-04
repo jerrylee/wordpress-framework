ed787df093c5ec664a46158b0b0185cd72d43526
webmasterintexas <webmasterintexas@gmail.com>
2016-01-04 16:45:19 -0600

    more stuff

--- partials/splash-image.php
+++ partials/splash-image.php
@@ -1,7 +1,15 @@
 <section id="splash-image">
   <div class="container">
-    <div class="row-fluid">
-      <?php get_template_part('partials/splash-image'); ?>
-    </div><!-- pulls either a carousel from splash-carousel.php or an image with animations from splash-image.php -->
+    <div class="row clearfix">
+      <div class="col-xs-6">
+        <div class="row-fluid animated slideInLeft one"><h3>Client Name</h3></div>
+        <div class="row-fluid animated slideInLeft two"><h3>Call To Action</h3></div>
+        <div class="row-fluid animated slideInUp three"><h3>000-000-0000</h3></div>
+      </div>
+      <div class="col-xs-6">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/client.png" alt="Client Picture" class="client-image" />
+        <!--TODO:upload client image and put path -->
+      </div>
+    </div><!-- row-fluid -->
   </div><!--/container-->
 </section><!--/splash-image-->
