114a96cc7da9c482cd6867a8e0841d6bfaeef9a9
webmasterintexas <webmasterintexas@gmail.com>
2016-01-04 16:03:17 -0600

    images

---
+++ partials/splash-carousel.php
@@ -0,0 +1,16 @@
+<section id="splash-carousel">
+  <div id="carousel-splash" class="carousel slide" data-ride="carousel">
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img src="..." alt="...">
+      </div><!--/item -->
+
+      <div class="item">
+        <img src="..." alt="...">
+      </div><!--/item -->
+
+    </div><!--/carousel inner -->
+  </div><!--/carousel splash-->
+</section><!--/splash-carousel-->
---
+++ partials/splash-image.php
@@ -0,0 +1,7 @@
+<section id="splash-image">
+  <div class="container">
+    <div class="row-fluid">
+      <?php get_template_part('partials/splash-image'); ?>
+    </div><!-- pulls either a carousel from splash-carousel.php or an image with animations from splash-image.php -->
+  </div><!--/container-->
+</section><!--/splash-image-->
